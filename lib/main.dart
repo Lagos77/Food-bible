ca82c467e9b079954c725705e9380f85a5e46659
Lagos77 <social_a@live.com>
2022-11-02 23:32:18 +0100

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foodbible/views/home.dart';
+import 'package:foodbible/mainpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.amber,
         primaryColor: Colors.white,
       ),
-      home: HomePage(),
+      home: MainPage(),
     );
   }
 }
