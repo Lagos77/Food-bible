a5fdb03cda380a641d5c3b6e3c5867d315767bcf
Toni <89582846+Noccis@users.noreply.github.com>
2022-11-02 02:31:14 -0700

    basic folders created and homepage

---
+++ lib/views/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+ 
+class HomePage extends StatefulWidget {
+ @override
+ State<HomePage> createState() => _HomePageState();
+}
+ 
+class _HomePageState extends State<HomePage> {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Row(children: [
+         Icon(Icons.restaurant_menu),
+         SizedBox(width: 10,),
+         Text('Food Bible')
+       ],),
+     ),
+   );
+ }
+}
