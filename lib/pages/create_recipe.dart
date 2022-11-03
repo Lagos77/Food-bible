8685d332fcb54586a43f58e6f813bc6b58a3de49
Toni <89582846+Noccis@users.noreply.github.com>
2022-11-03 05:55:21 -0700

    added checkboxes for tags

--- lib/pages/create_recipe.dart
+++ lib/pages/create_recipe.dart
@@ -8,10 +8,18 @@ class CreateRecipePage extends StatefulWidget {
 
 class _CreateRecipePageState extends State<CreateRecipePage> {
 
+// Text controllers for the input
   final _nameController = TextEditingController();
   final _descriptionController = TextEditingController();
+
+  // Bools for the different tags
+  bool? isVegetarianChecked = false;
+  bool? isMealChecked = false;
+  bool? isDesertChecked = false;
+  bool? isGlutenfreeChecked = false;
   
 
+// Function to save the recipe
   void saveNewRecipe() {
     final recipeName = _nameController.text;
     final recipeDescription = _descriptionController.text;
@@ -35,6 +43,7 @@ class _CreateRecipePageState extends State<CreateRecipePage> {
           }, icon: Icon(Icons.clear)),
         hintText: "Namn på recept"),
       ),
+      const Padding(padding: EdgeInsets.only(bottom: 30)),
       TextField(
         controller: _descriptionController,
         decoration: InputDecoration(
@@ -45,11 +54,56 @@ class _CreateRecipePageState extends State<CreateRecipePage> {
           }, icon: Icon(Icons.clear)),
         hintText: "Beskrivning"),
       ),
+      Padding(padding: EdgeInsets.all(20)),
+      Row(children: [
+        Checkbox(
+        value: isVegetarianChecked, 
+        onChanged: (newBool) {
+        setState(() {
+          isVegetarianChecked = newBool;
+        });
+      }),
+      Text("Vegetarisk")
+      ],),
+      Row(
+        children: [
+          Checkbox(
+        value: isGlutenfreeChecked, 
+        onChanged: (newBool) {
+        setState(() {
+          isGlutenfreeChecked = newBool;
+        });
+      }),
+      Text("Glutenfritt")
+        ],
+      ),
+      Row(children: [
+        Checkbox(
+        value: isDesertChecked, 
+        onChanged: (newBool) {
+        setState(() {
+          isDesertChecked = newBool;
+        });
+      }),
+      Text("Efterätt")
+      ],),
+      Row(children: [
+        Checkbox(
+        value: isMealChecked, 
+        onChanged: (newBool) {
+        setState(() {
+          isMealChecked = newBool;
+        });
+      }),
+      Text("Måltid")
+      ],),
+      Spacer(),
       MaterialButton(
         onPressed: () {},
         color: Colors.amber,
         child: const Text('Spara', style: TextStyle(color: Colors.black),),
-        )
+        ),
+        Padding(padding: EdgeInsets.only(bottom: 30))
     ],
    );
   }
