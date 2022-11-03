ce1c0517b77d6d2158c3811053e0b74d9939e5f7
Toni <89582846+Noccis@users.noreply.github.com>
2022-11-03 06:19:25 -0700

    added avbryt button and changed design a bit

--- lib/pages/create_recipe.dart
+++ lib/pages/create_recipe.dart
@@ -23,88 +23,102 @@ class _CreateRecipePageState extends State<CreateRecipePage> {
   void saveNewRecipe() {
     final recipeName = _nameController.text;
     final recipeDescription = _descriptionController.text;
-
-    Navigator.of(context).pop();
-
   }
   @override
   Widget build(BuildContext context) {
-    return Column( 
-     children: [
-      Padding(padding: EdgeInsets.all(40.0)),
-      Text("Skapa nytt recept"),
-      TextField(
-        controller: _nameController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          suffixIcon: IconButton(
-            onPressed: () {
-              _nameController.clear();
-          }, icon: Icon(Icons.clear)),
-        hintText: "Namn på recept"),
-      ),
-      const Padding(padding: EdgeInsets.only(bottom: 30)),
-      TextField(
-        controller: _descriptionController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          suffixIcon: IconButton(
-            onPressed: () {
-              _nameController.clear();
-          }, icon: Icon(Icons.clear)),
-        hintText: "Beskrivning"),
-      ),
-      Padding(padding: EdgeInsets.all(20)),
-      Row(children: [
-        Checkbox(
-        value: isVegetarianChecked, 
-        onChanged: (newBool) {
-        setState(() {
-          isVegetarianChecked = newBool;
-        });
-      }),
-      Text("Vegetarisk")
-      ],),
-      Row(
-        children: [
-          Checkbox(
-        value: isGlutenfreeChecked, 
-        onChanged: (newBool) {
-        setState(() {
-          isGlutenfreeChecked = newBool;
-        });
-      }),
-      Text("Glutenfritt")
-        ],
-      ),
-      Row(children: [
-        Checkbox(
-        value: isDesertChecked, 
-        onChanged: (newBool) {
-        setState(() {
-          isDesertChecked = newBool;
-        });
-      }),
-      Text("Efterätt")
-      ],),
-      Row(children: [
-        Checkbox(
-        value: isMealChecked, 
-        onChanged: (newBool) {
-        setState(() {
-          isMealChecked = newBool;
-        });
-      }),
-      Text("Måltid")
-      ],),
-      Spacer(),
-      MaterialButton(
-        onPressed: () {},
-        color: Colors.amber,
-        child: const Text('Spara', style: TextStyle(color: Colors.black),),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column( 
+       children: [
+        const Padding(padding: EdgeInsets.all(40.0)),
+        Text("Skapa nytt recept"),
+        const Padding(padding: EdgeInsets.only(bottom: 30)),
+        TextField(
+          controller: _nameController,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            suffixIcon: IconButton(
+              onPressed: () {
+                _nameController.clear();
+            }, icon: const Icon(Icons.clear)),
+          hintText: "Namn på recept"),
         ),
-        Padding(padding: EdgeInsets.only(bottom: 30))
-    ],
-   );
+        const Padding(padding: EdgeInsets.only(bottom: 30)),
+        TextField(
+          controller: _descriptionController,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            suffixIcon: IconButton(
+              onPressed: () {
+                _descriptionController.clear();
+            }, icon: const Icon(Icons.clear)),
+          hintText: "Beskrivning"),
+        ),
+        const Padding(padding: EdgeInsets.all(20)),
+        Row(children: [
+          Checkbox(
+          value: isVegetarianChecked, 
+          onChanged: (newBool) {
+          setState(() {
+            isVegetarianChecked = newBool;
+          });
+        }),
+        const Text("Vegetarisk")
+        ],),
+        Row(
+          children: [
+            Checkbox(
+          value: isGlutenfreeChecked, 
+          onChanged: (newBool) {
+          setState(() {
+            isGlutenfreeChecked = newBool;
+          });
+        }),
+        const Text("Glutenfritt")
+          ],
+        ),
+        Row(children: [
+          Checkbox(
+          value: isDesertChecked, 
+          onChanged: (newBool) {
+          setState(() {
+            isDesertChecked = newBool;
+          });
+        }),
+        const Text("Efterätt")
+        ],),
+        Row(children: [
+          Checkbox(
+          value: isMealChecked, 
+          onChanged: (newBool) {
+          setState(() {
+            isMealChecked = newBool;
+          });
+        }),
+        const Text("Måltid")
+        ],),
+        const Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.amber,
+              child: const Text('Spara', style: TextStyle(color: Colors.black),),
+              ),
+              const Padding(padding: EdgeInsets.only(right: 50)),
+              MaterialButton(
+              onPressed: () {
+                // Navigera hem
+              },
+              color: Colors.red,
+              child: const Text('Avbryt', style: TextStyle(color: Colors.black),),
+              ),
+          ],
+        ),
+          const Padding(padding: EdgeInsets.only(bottom: 30))
+      ],
+   ),
+    );
   }
 }
